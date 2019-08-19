8151155cefe12a666c4bad0d88ff4635ea2fbdc1
Scott Daniels <sdaniels6363@gmail.com>
2019-08-19 17:52:30 -0400

    So many changes can't list them all, but I did make the gifs animate after being clicked and they load dynamically.  Need to fix the topic addition.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,9 @@
 var topics = ["wrc", "lfc", "rallycross", "drifting", "imsa", "mr-robot"]
 
-$(document).ready(function () {
-
+function loadTopics() {
+  $("#topics").empty();
   for (i = 0; i < topics.length; i++) {
     newButton = $("<button>");
-    console.log(newButton);
     // add topic to id attribute
     newButton.attr("id", topics[i]);
     // add bootstrap btn class
@@ -13,43 +12,68 @@ $(document).ready(function () {
     newButton.text(topics[i]);
     // Lastly append the new button to #gifs
     $("#topics").append(newButton);
-    console.log("added button "+topics[i]);
   }
-  
-  
 
-  $(".btn").on("click",function () {
+  $("#gif-search").on("click", function () {
+    event.preventDefault();
+    // This line of code will grab the input from the textbox
+    var topic = $("#gif-search").val().trim();
+
+    // The movie from the textbox is then added to our array
+    topics.push(topic);
+
+    // Calling renderButtons which handles the processing of our movie array
+    loadTopics();
+
+  });
+}
+
+$(document).ready(function () {
+
+  loadTopics(); // loads initial topics
+
+  $(".btn").on("click", function () {
     var baseURL = "https://api.giphy.com/v1/gifs/search?api_key=OXaAQusBTQE0O5Sl8XgfGrioVNpsQBj3&limit=10&q=";
-  
     var queryURL = baseURL + this.id;
-  
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
-  
+
       var data = response.data;
-  
+
       for (i = 0; i < data.length; i++) {
-        var staticGIF = data[i].images.fixed_height_still.url;
+        var stillGIF = data[i].images.fixed_height_still.url;
         var animatedGIF = data[i].images.fixed_height.url;
-  
+
         // create gif element
-        var newGif = $("<img>").attr("data-state","still")
-        newGif.attr("src", staticGIF);
-        newGif.attr("data-static",staticGIF);
-        newGif.attr("data-animated",animatedGIF);
-  
-        newGif.appendTo("#gifs");
+        var newGif = $("<img>");
+        newGif.attr("data-state", "still");
+        newGif.attr("src", stillGIF); // image on page is static by default
+        newGif.attr("data-still", stillGIF); // source url of static image
+        newGif.attr("data-animated", animatedGIF); // source url of animated image
+        newGif.addClass("gif") // add the gif class to the image
+        newGif.appendTo("#gifs"); // add to the gifs w
       }
-  
+
+      $(".gif").on("click", function () {
+        var gif = $(this);
+        var animated = gif.attr("data-animated");
+        var still = gif.attr("data-still");
+
+        if (gif.attr("data-state") === "still") {
+          gif.attr("src", animated);
+          gif.attr("data-state", "animated");
+        } else {
+          gif.attr("src", still);
+          gif.attr("data-state", "still");
+        }
+      });
+
+
     });
+
+
   });
-
-
-
-
-
-
-
 });
+
